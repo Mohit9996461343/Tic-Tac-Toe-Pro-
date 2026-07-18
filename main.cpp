8acc91b9b745732f101f4bbdc3fcc4b4ba4823d1
Mohit Verma <mohitkumarverma024@gmail.com>
2026-07-18 21:49:00 +0530

    Added main menu and instructions system

--- main.cpp
+++ main.cpp
@@ -3,26 +3,63 @@
 
 using namespace std;
 
+void showInstructions()
+{
+    cout << "\n===== HOW TO PLAY =====\n";
+    cout << "1. Player X starts the game.\n";
+    cout << "2. Enter row and column numbers (1-3).\n";
+    cout << "3. First player to make 3 symbols in a row wins.\n";
+    cout << "4. Have fun!\n\n";
+}
+
 int main()
 {
-    cout << "============================\n";
-    cout << "       TIC TAC TOE PRO       \n";
-    cout << "============================\n\n";
+    int choice;
 
-    string name1, name2;
+    while(true)
+    {
+        cout << "\n============================\n";
+        cout << "       TIC TAC TOE PRO       \n";
+        cout << "============================\n";
 
-    cout << "Enter Player 1 name: ";
-    cin >> name1;
+        cout << "1. Start New Game\n";
+        cout << "2. How to Play\n";
+        cout << "3. Exit\n";
 
-    cout << "Enter Player 2 name: ";
-    cin >> name2;
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-    Player player1(name1, 'X');
-    Player player2(name2, 'O');
+        if(choice == 1)
+        {
+            string name1, name2;
 
-    Game game(player1, player2);
+            cout << "\nEnter Player 1 name: ";
+            cin >> name1;
 
-    game.startGame();
+            cout << "Enter Player 2 name: ";
+            cin >> name2;
+
+            Player player1(name1, 'X');
+            Player player2(name2, 'O');
+
+            Game game(player1, player2);
+
+            game.startGame();
+        }
+        else if(choice == 2)
+        {
+            showInstructions();
+        }
+        else if(choice == 3)
+        {
+            cout << "Thanks for playing!\n";
+            break;
+        }
+        else
+        {
+            cout << "Invalid choice!\n";
+        }
+    }
 
     return 0;
 }
